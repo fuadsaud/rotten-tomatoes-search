ca79fc832774df592d20a0220c8fe2156969332c
Fuad Saud <fuadfsaud@gmail.com>
2015-03-12 03:50:19 -0400

    Allow cross domain access and fix queries

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require('express');
+const morgan  = require('morgan');
 const mongo   = require('mongodb').MongoClient;
 
 const mongoURL = 'mongodb://localhost/rottentomatoes';
 
 const app = express();
 
-app.get('/movies/search', function(req, res) {
+app.use(morgan('dev'));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
+app.get('/movies', function(req, res) {
     const query = req.query.q;
+
+    if (!query) {
+        res.json({ movies: [] });
+
+        return;
+    }
 
     mongo.connect(mongoURL, function(err, db) {
         if (err) console.log(err);
@@ -14,7 +29,7 @@ app.get('/movies/search', function(req, res) {
         const movies = db.collection('movies');
 
         movies.find({ title: new RegExp(query, 'i') }).toArray(function(err, docs) {
-            res.json(docs);
+            res.json({ movies: docs });
 
             db.close();
         });
@@ -31,7 +46,7 @@ app.get('/movies/:movie_id', function(req, res) {
 
         movies.findOne({ id: movieId }, { limit: 1 }, function(err, doc) {
             if (doc)
-                res.json(doc);
+                res.json({ movie: doc });
             else
                 res.status(404).send('Not found.');
 

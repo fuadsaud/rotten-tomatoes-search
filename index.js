571cfcdbc4bc4fbef560b6c8a15a46de3dcf55f3
Fuad Saud <fuadfsaud@gmail.com>
2015-03-12 02:19:36 -0400

    Make search endpoint query mongo correctly

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const mongo   = require('mongodb').MongoClient;
+
+const mongoURL = 'mongodb://localhost/rottentomatoes';
 
 const app = express();
 
-app.get('/movies', function(req, res) {
-    res.json([{ id: 1, title: 'Hey there!' }]);
+app.get('/movies/search', function(req, res) {
+    const query = req.query.q;
+
+    mongo.connect(mongoURL, function(err, db) {
+        if (err) console.log(err);
+
+        const movies = db.collection('movies');
+
+        movies.find({ title: new RegExp(query, 'i') }).toArray(function(err, docs) {
+            res.json(docs);
+
+            db.close();
+        });
+    });
 });
 
 app.listen(7000);

2b31b9cbb0502154b7795b628cb447583da2bc38
Fuad Saud <fuadfsaud@gmail.com>
2015-03-12 02:33:49 -0400

    Add get endpoint for movie resource

--- index.js
+++ index.js
@@ -21,4 +21,23 @@ app.get('/movies/search', function(req, res) {
     });
 });
 
+app.get('/movies/:movie_id', function(req, res) {
+    const movieId = req.params.movie_id;
+
+    mongo.connect(mongoURL, function(err, db) {
+        if (err) console.log(err);
+
+        const movies = db.collection('movies');
+
+        movies.findOne({ id: movieId }, { limit: 1 }, function(err, doc) {
+            if (doc)
+                res.json(doc);
+            else
+                res.status(404).send('Not found.');
+
+            db.close();
+        });
+    });
+});
+
 app.listen(7000);
